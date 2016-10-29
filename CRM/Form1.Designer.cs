71a889ec34b1a4155da6adf982621e7b0dba1c41
maciejkozikowski <maciej.kozik94@gmail.com>
2016-10-29 14:37:53 +0200

    2

--- CRM/Form1.Designer.cs
+++ CRM/Form1.Designer.cs
@@ -28,12 +28,197 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelpesel = new System.Windows.Forms.Label();
+            this.labelImie = new System.Windows.Forms.Label();
+            this.labelNazwisko = new System.Windows.Forms.Label();
+            this.labelAdresZameldowania = new System.Windows.Forms.Label();
+            this.labelAdresKorespondencyjny = new System.Windows.Forms.Label();
+            this.labelTelefonKontaktowy = new System.Windows.Forms.Label();
+            this.labelEMail = new System.Windows.Forms.Label();
+            this.textBoxPesel = new System.Windows.Forms.TextBox();
+            this.textBoxImie = new System.Windows.Forms.TextBox();
+            this.textBoxNazwisko = new System.Windows.Forms.TextBox();
+            this.textBoxAdresZameldowania = new System.Windows.Forms.TextBox();
+            this.textBoxAdresKorespondencyjny = new System.Windows.Forms.TextBox();
+            this.textBoxTelefonKontaktowy = new System.Windows.Forms.TextBox();
+            this.textBoxEMail = new System.Windows.Forms.TextBox();
+            this.buttonDodajKlienta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelpesel
+            // 
+            this.labelpesel.AutoSize = true;
+            this.labelpesel.Location = new System.Drawing.Point(13, 13);
+            this.labelpesel.Name = "labelpesel";
+            this.labelpesel.Size = new System.Drawing.Size(32, 13);
+            this.labelpesel.TabIndex = 0;
+            this.labelpesel.Text = "pesel";
+            // 
+            // labelImie
+            // 
+            this.labelImie.AutoSize = true;
+            this.labelImie.Location = new System.Drawing.Point(13, 30);
+            this.labelImie.Name = "labelImie";
+            this.labelImie.Size = new System.Drawing.Size(26, 13);
+            this.labelImie.TabIndex = 1;
+            this.labelImie.Text = "Imię";
+            // 
+            // labelNazwisko
+            // 
+            this.labelNazwisko.AutoSize = true;
+            this.labelNazwisko.Location = new System.Drawing.Point(13, 47);
+            this.labelNazwisko.Name = "labelNazwisko";
+            this.labelNazwisko.Size = new System.Drawing.Size(53, 13);
+            this.labelNazwisko.TabIndex = 2;
+            this.labelNazwisko.Text = "Nazwisko";
+            // 
+            // labelAdresZameldowania
+            // 
+            this.labelAdresZameldowania.AutoSize = true;
+            this.labelAdresZameldowania.Location = new System.Drawing.Point(13, 64);
+            this.labelAdresZameldowania.Name = "labelAdresZameldowania";
+            this.labelAdresZameldowania.Size = new System.Drawing.Size(104, 13);
+            this.labelAdresZameldowania.TabIndex = 3;
+            this.labelAdresZameldowania.Text = "Adres zameldowania";
+            // 
+            // labelAdresKorespondencyjny
+            // 
+            this.labelAdresKorespondencyjny.AutoSize = true;
+            this.labelAdresKorespondencyjny.Location = new System.Drawing.Point(13, 81);
+            this.labelAdresKorespondencyjny.Name = "labelAdresKorespondencyjny";
+            this.labelAdresKorespondencyjny.Size = new System.Drawing.Size(123, 13);
+            this.labelAdresKorespondencyjny.TabIndex = 4;
+            this.labelAdresKorespondencyjny.Text = "Adres korespondencyjny";
+            // 
+            // labelTelefonKontaktowy
+            // 
+            this.labelTelefonKontaktowy.AutoSize = true;
+            this.labelTelefonKontaktowy.Location = new System.Drawing.Point(13, 98);
+            this.labelTelefonKontaktowy.Name = "labelTelefonKontaktowy";
+            this.labelTelefonKontaktowy.Size = new System.Drawing.Size(101, 13);
+            this.labelTelefonKontaktowy.TabIndex = 5;
+            this.labelTelefonKontaktowy.Text = "Telefon kontaktowy";
+            // 
+            // labelEMail
+            // 
+            this.labelEMail.AutoSize = true;
+            this.labelEMail.Location = new System.Drawing.Point(13, 115);
+            this.labelEMail.Name = "labelEMail";
+            this.labelEMail.Size = new System.Drawing.Size(35, 13);
+            this.labelEMail.TabIndex = 6;
+            this.labelEMail.Text = "E-mail";
+            // 
+            // textBoxPesel
+            // 
+            this.textBoxPesel.Location = new System.Drawing.Point(159, 5);
+            this.textBoxPesel.MaxLength = 11;
+            this.textBoxPesel.Name = "textBoxPesel";
+            this.textBoxPesel.Size = new System.Drawing.Size(155, 20);
+            this.textBoxPesel.TabIndex = 7;
+            // 
+            // textBoxImie
+            // 
+            this.textBoxImie.Location = new System.Drawing.Point(159, 22);
+            this.textBoxImie.MaxLength = 20;
+            this.textBoxImie.Name = "textBoxImie";
+            this.textBoxImie.Size = new System.Drawing.Size(155, 20);
+            this.textBoxImie.TabIndex = 8;
+            // 
+            // textBoxNazwisko
+            // 
+            this.textBoxNazwisko.Location = new System.Drawing.Point(159, 39);
+            this.textBoxNazwisko.MaxLength = 40;
+            this.textBoxNazwisko.Name = "textBoxNazwisko";
+            this.textBoxNazwisko.Size = new System.Drawing.Size(155, 20);
+            this.textBoxNazwisko.TabIndex = 9;
+            // 
+            // textBoxAdresZameldowania
+            // 
+            this.textBoxAdresZameldowania.Location = new System.Drawing.Point(159, 56);
+            this.textBoxAdresZameldowania.MaxLength = 255;
+            this.textBoxAdresZameldowania.Name = "textBoxAdresZameldowania";
+            this.textBoxAdresZameldowania.Size = new System.Drawing.Size(155, 20);
+            this.textBoxAdresZameldowania.TabIndex = 10;
+            // 
+            // textBoxAdresKorespondencyjny
+            // 
+            this.textBoxAdresKorespondencyjny.Location = new System.Drawing.Point(159, 73);
+            this.textBoxAdresKorespondencyjny.MaxLength = 255;
+            this.textBoxAdresKorespondencyjny.Name = "textBoxAdresKorespondencyjny";
+            this.textBoxAdresKorespondencyjny.Size = new System.Drawing.Size(155, 20);
+            this.textBoxAdresKorespondencyjny.TabIndex = 11;
+            // 
+            // textBoxTelefonKontaktowy
+            // 
+            this.textBoxTelefonKontaktowy.Location = new System.Drawing.Point(159, 90);
+            this.textBoxTelefonKontaktowy.MaxLength = 11;
+            this.textBoxTelefonKontaktowy.Name = "textBoxTelefonKontaktowy";
+            this.textBoxTelefonKontaktowy.Size = new System.Drawing.Size(155, 20);
+            this.textBoxTelefonKontaktowy.TabIndex = 12;
+            this.textBoxTelefonKontaktowy.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBoxEMail
+            // 
+            this.textBoxEMail.Location = new System.Drawing.Point(159, 107);
+            this.textBoxEMail.MaxLength = 50;
+            this.textBoxEMail.Name = "textBoxEMail";
+            this.textBoxEMail.Size = new System.Drawing.Size(155, 20);
+            this.textBoxEMail.TabIndex = 13;
+            // 
+            // buttonDodajKlienta
+            // 
+            this.buttonDodajKlienta.Location = new System.Drawing.Point(161, 176);
+            this.buttonDodajKlienta.Name = "buttonDodajKlienta";
+            this.buttonDodajKlienta.Size = new System.Drawing.Size(98, 23);
+            this.buttonDodajKlienta.TabIndex = 14;
+            this.buttonDodajKlienta.Text = "Dodaj klienta";
+            this.buttonDodajKlienta.UseVisualStyleBackColor = true;
+            this.buttonDodajKlienta.Click += new System.EventHandler(this.buttonDodajKlienta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 261);
+            this.Controls.Add(this.buttonDodajKlienta);
+            this.Controls.Add(this.textBoxEMail);
+            this.Controls.Add(this.textBoxTelefonKontaktowy);
+            this.Controls.Add(this.textBoxAdresKorespondencyjny);
+            this.Controls.Add(this.textBoxAdresZameldowania);
+            this.Controls.Add(this.textBoxNazwisko);
+            this.Controls.Add(this.textBoxImie);
+            this.Controls.Add(this.textBoxPesel);
+            this.Controls.Add(this.labelEMail);
+            this.Controls.Add(this.labelTelefonKontaktowy);
+            this.Controls.Add(this.labelAdresKorespondencyjny);
+            this.Controls.Add(this.labelAdresZameldowania);
+            this.Controls.Add(this.labelNazwisko);
+            this.Controls.Add(this.labelImie);
+            this.Controls.Add(this.labelpesel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelpesel;
+        private System.Windows.Forms.Label labelImie;
+        private System.Windows.Forms.Label labelNazwisko;
+        private System.Windows.Forms.Label labelAdresZameldowania;
+        private System.Windows.Forms.Label labelAdresKorespondencyjny;
+        private System.Windows.Forms.Label labelTelefonKontaktowy;
+        private System.Windows.Forms.Label labelEMail;
+        private System.Windows.Forms.TextBox textBoxPesel;
+        private System.Windows.Forms.TextBox textBoxImie;
+        private System.Windows.Forms.TextBox textBoxNazwisko;
+        private System.Windows.Forms.TextBox textBoxAdresZameldowania;
+        private System.Windows.Forms.TextBox textBoxAdresKorespondencyjny;
+        private System.Windows.Forms.TextBox textBoxTelefonKontaktowy;
+        private System.Windows.Forms.TextBox textBoxEMail;
+        private System.Windows.Forms.Button buttonDodajKlienta;
     }
 }
 
--- CRM/Form1.cs
+++ CRM/Form1.cs
@@ -5,14 +5,36 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CRM
 {
     public partial class Form1 : Form
     {
+        public string connectionString = "Data Source=DESKTOP-M9RN1U7\\SQLEXPRESS;Initial Catalog=CRM;Integrated Security=True;Pooling=False";
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDodajKlienta_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            if (con.State==System.Data.ConnectionState.Open)
+            {
+                string q = "INSERT INTO klient VALUES ('" + textBoxPesel.Text.ToString() + "', '" + textBoxImie.Text.ToString() + "', '" + textBoxNazwisko.Text.ToString() + "', '" + textBoxAdresZameldowania.Text.ToString() + "', '" + textBoxAdresKorespondencyjny.Text.ToString() + "', '" + textBoxTelefonKontaktowy.Text.ToString() + "', '" + textBoxEMail.Text.ToString() + "');";
+                SqlCommand cmd = new SqlCommand(q, con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Dodano klienta");
+            }
         }
     }
 }
