6018778abdbb18e79feabd313bd89a5953c6b34c
theSpawacz <klatka25@gmail.com>
2016-11-03 21:51:49 +0100

    no porobione, podlaczenie do bazy, oddzielnla klasa do tego

--- CRM/Form1.cs
+++ CRM/Form1.cs
@@ -5,17 +5,22 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-using System.Data.SqlClient;
+
+using MySql.Data.MySqlClient;
 
 namespace CRM
 {
     public partial class Form1 : Form
     {
-        public string connectionString = "Data Source=DESKTOP-M9RN1U7\\SQLEXPRESS;Initial Catalog=CRM;Integrated Security=True;Pooling=False";
         
+       
+        
+
+
         public Form1()
         {
             InitializeComponent();
+            SqlConnectionClass.Foo();
         }
 
 
@@ -26,19 +31,37 @@ namespace CRM
 
         private void buttonDodajKlienta_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(connectionString);
-            con.Open();
+            
+                string sql = "Create table test(id int(4);";
+                MySqlCommand cmd = new MySqlCommand(sql, SqlConnectionClass.myConnection);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    MessageBox.Show(rdr[0] + " -- " + rdr[1]);
+                }
+                rdr.Close();
+ 
+
+
+
+
+
+            /*  -----------     sql
+            
             if (con.State==System.Data.ConnectionState.Open)
             {
                 string q = "INSERT INTO klient VALUES ('" + textBoxPesel.Text.ToString() + "', '" + textBoxImie.Text.ToString() + "', '" + textBoxNazwisko.Text.ToString() + "', '" + textBoxAdresZameldowania.Text.ToString() + "', '" + textBoxAdresKorespondencyjny.Text.ToString() + "', '" + textBoxTelefonKontaktowy.Text.ToString() + "', '" + textBoxEMail.Text.ToString() + "');";
                 SqlCommand cmd = new SqlCommand(q, con);
                 cmd.ExecuteNonQuery();
                 MessageBox.Show("Dodano klienta");
-            }
-        }
+            }*/
+
+        }   
+           
 
         private void buttonWyszukajPesel_Click(object sender, EventArgs e)
-        {
+        {/*
             SqlConnection con = new SqlConnection(connectionString);
             con.Open();
             using (SqlCommand command = new SqlCommand("select * from klient where pesel='" + textBoxPesel.Text.ToString() + "'", con))
@@ -56,7 +79,13 @@ namespace CRM
                         textBoxEMail.Text = reader["email"].ToString();
                     }
                 }
-            }
+            }*/
         }
     }
+
+
+
+   
+
 }
+
---
+++ CRM/SqlConnectionClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace CRM
+{
+    class SqlConnectionClass
+    {
+
+        private static string myConnectionString = "server=mysql3.gear.host;uid=crmproject;" + "pwd=zaq1@WSX;database=crmproject;";
+        public static MySqlConnection myConnection = new MySqlConnection(myConnectionString);
+
+
+        public static void Foo(){
+            try
+        {
+                MessageBox.Show("Connecting to MySQL...");
+                myConnection.Open();
+
+           }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Nie można się połączyć z bazą danych!");
+                        break;
+                    case 1045:
+                        MessageBox.Show("Zła nazwa/hasło użytkownika bazy danych");
+                        break;
+                }
+            }
+
+        }
+        
+
+
+    }
+}
