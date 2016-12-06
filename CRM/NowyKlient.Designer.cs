12d744b4cc6b52eedc8f0e2a4f3dae88d6815638
theSpawacz <klatka25@gmail.com>
2016-12-06 22:22:07 +0100

    fix

---
+++ CRM/NowyKlient.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace CRM
+{
+    partial class NowyKlient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NowyKlient));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkZgoda1 = new System.Windows.Forms.CheckBox();
+            this.checkZgoda2 = new System.Windows.Forms.CheckBox();
+            this.checkZgoda3 = new System.Windows.Forms.CheckBox();
+            this.imieTextBox = new System.Windows.Forms.TextBox();
+            this.nazwiskoTextBox = new System.Windows.Forms.TextBox();
+            this.peselTextBox = new System.Windows.Forms.TextBox();
+            this.adresZameldowaniaTextBox = new System.Windows.Forms.TextBox();
+            this.adresKorespondencyjnyTextBox = new System.Windows.Forms.TextBox();
+            this.telefonTextBox = new System.Windows.Forms.TextBox();
+            this.eMailTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Imię";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nazwisko";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Numer PESEL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Adres zameldowania";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Adres korespondencyjny";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Telefon kontaktowy";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 195);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Adres e-mail";
+            // 
+            // checkZgoda1
+            // 
+            this.checkZgoda1.AutoSize = true;
+            this.checkZgoda1.Location = new System.Drawing.Point(16, 227);
+            this.checkZgoda1.Name = "checkZgoda1";
+            this.checkZgoda1.Size = new System.Drawing.Size(92, 17);
+            this.checkZgoda1.TabIndex = 7;
+            this.checkZgoda1.Text = "Przetwarzanie";
+            this.checkZgoda1.UseVisualStyleBackColor = true;
+            // 
+            // checkZgoda2
+            // 
+            this.checkZgoda2.AutoSize = true;
+            this.checkZgoda2.Location = new System.Drawing.Point(116, 227);
+            this.checkZgoda2.Name = "checkZgoda2";
+            this.checkZgoda2.Size = new System.Drawing.Size(73, 17);
+            this.checkZgoda2.TabIndex = 8;
+            this.checkZgoda2.Text = "Marketing";
+            this.checkZgoda2.UseVisualStyleBackColor = true;
+            // 
+            // checkZgoda3
+            // 
+            this.checkZgoda3.AutoSize = true;
+            this.checkZgoda3.Location = new System.Drawing.Point(216, 227);
+            this.checkZgoda3.Name = "checkZgoda3";
+            this.checkZgoda3.Size = new System.Drawing.Size(72, 17);
+            this.checkZgoda3.TabIndex = 9;
+            this.checkZgoda3.Text = "E-Faktura";
+            this.checkZgoda3.UseVisualStyleBackColor = true;
+            // 
+            // imieTextBox
+            // 
+            this.imieTextBox.Location = new System.Drawing.Point(167, 12);
+            this.imieTextBox.Name = "imieTextBox";
+            this.imieTextBox.Size = new System.Drawing.Size(100, 20);
+            this.imieTextBox.TabIndex = 10;
+            // 
+            // nazwiskoTextBox
+            // 
+            this.nazwiskoTextBox.Location = new System.Drawing.Point(167, 42);
+            this.nazwiskoTextBox.Name = "nazwiskoTextBox";
+            this.nazwiskoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nazwiskoTextBox.TabIndex = 11;
+            // 
+            // peselTextBox
+            // 
+            this.peselTextBox.Location = new System.Drawing.Point(167, 72);
+            this.peselTextBox.Name = "peselTextBox";
+            this.peselTextBox.Size = new System.Drawing.Size(100, 20);
+            this.peselTextBox.TabIndex = 12;
+            // 
+            // adresZameldowaniaTextBox
+            // 
+            this.adresZameldowaniaTextBox.Location = new System.Drawing.Point(167, 105);
+            this.adresZameldowaniaTextBox.Name = "adresZameldowaniaTextBox";
+            this.adresZameldowaniaTextBox.Size = new System.Drawing.Size(237, 20);
+            this.adresZameldowaniaTextBox.TabIndex = 13;
+            // 
+            // adresKorespondencyjnyTextBox
+            // 
+            this.adresKorespondencyjnyTextBox.Location = new System.Drawing.Point(167, 135);
+            this.adresKorespondencyjnyTextBox.Name = "adresKorespondencyjnyTextBox";
+            this.adresKorespondencyjnyTextBox.Size = new System.Drawing.Size(237, 20);
+            this.adresKorespondencyjnyTextBox.TabIndex = 14;
+            // 
+            // telefonTextBox
+            // 
+            this.telefonTextBox.Location = new System.Drawing.Point(167, 165);
+            this.telefonTextBox.Name = "telefonTextBox";
+            this.telefonTextBox.Size = new System.Drawing.Size(237, 20);
+            this.telefonTextBox.TabIndex = 15;
+            // 
+            // eMailTextBox
+            // 
+            this.eMailTextBox.Location = new System.Drawing.Point(167, 192);
+            this.eMailTextBox.Name = "eMailTextBox";
+            this.eMailTextBox.Size = new System.Drawing.Size(237, 20);
+            this.eMailTextBox.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(289, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Dodaj klienta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(302, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Wyjdź";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(289, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Wygeneruj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // NowyKlient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 267);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.eMailTextBox);
+            this.Controls.Add(this.telefonTextBox);
+            this.Controls.Add(this.adresKorespondencyjnyTextBox);
+            this.Controls.Add(this.adresZameldowaniaTextBox);
+            this.Controls.Add(this.peselTextBox);
+            this.Controls.Add(this.nazwiskoTextBox);
+            this.Controls.Add(this.imieTextBox);
+            this.Controls.Add(this.checkZgoda3);
+            this.Controls.Add(this.checkZgoda2);
+            this.Controls.Add(this.checkZgoda1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "NowyKlient";
+            this.Text = "Formularz tworzenia nowego klienta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkZgoda1;
+        private System.Windows.Forms.CheckBox checkZgoda2;
+        private System.Windows.Forms.CheckBox checkZgoda3;
+        private System.Windows.Forms.TextBox imieTextBox;
+        private System.Windows.Forms.TextBox nazwiskoTextBox;
+        private System.Windows.Forms.TextBox peselTextBox;
+        private System.Windows.Forms.TextBox adresZameldowaniaTextBox;
+        private System.Windows.Forms.TextBox adresKorespondencyjnyTextBox;
+        private System.Windows.Forms.TextBox telefonTextBox;
+        private System.Windows.Forms.TextBox eMailTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ CRM/NowyKlient.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CRM
+{
+    public partial class NowyKlient : Form
+    {
+        public NowyKlient()
+        {
+            InitializeComponent();
+
+            //Niszczy obiekt po zamknięciu
+            this.FormClosing += NowyKlient_FormClosing;
+
+            //Tooltipy
+            ToolTip checkTop1 = new ToolTip();
+            checkTop1.SetToolTip(checkZgoda1, "Wyrażam zgodę na przetwarzanie moich danych osobowych" + Environment.NewLine + " dla potrzeb niezbędnych do realizacji świadczonych usług.");
+                  
+            ToolTip checkTop2 = new ToolTip();
+            checkTop2.SetToolTip(checkZgoda2, "Wyrażam zgodę na przetwarzanie danych transmisyjnych " + Environment.NewLine + "dla celów marketingu usług telekomunikacyjnych. Dane będą przetwarzane w okresie obowiązywania umowy.");
+
+            ToolTip checkTop3 = new ToolTip();
+            checkTop3.SetToolTip(checkZgoda3, "Wyrażam zgodę na realizację dostarczania faktur " + Environment.NewLine + "za świadczone usługi drogą elektroniczną.");
+
+
+        }
+
+        //Funkcja generująca klientow randowmowo
+        public void Generacja()
+        {
+            #region BazaDanychRegion
+            string[] ImionaMeskieTablica = new string[] { "Marian", "Kamil", "Maciek", "Bartek", "Adam", "Jan", "Paweł" };
+            string[] ImionaZenskieTablica = new string[] { 
+               "Ada", "Adela", "Adelajda", "Adrianna", "Agata", "Agnieszka", "Aldona", "Aleksandra", "Alicja", "Alina", "Amanda", "Amelia", "Anastazja", "Andżelika", "Aneta", "Anita", "Anna", "Antonina",
+               "Barbara","Beata","Berenika","Bernadeta","Blanka","Bogusława","Bożena",
+               "Cecylia","Celina","Czesława",
+               "Dagmara","Danuta","Daria","Diana","Dominika","Dorota",
+               "Edyta","Eliza","Elwira","Elżbieta"," Emilia","Eugenia","Ewa","Ewelina",
+               "Felicja","Franciszka",
+               "Gabriela","Grażyna",
+               "Halina","Hanna","Helena",
+               "Iga","Ilona","Irena","Irmina","Iwona","Izabela",
+               "Jadwiga","Janina","Joanna","Jolanta","Jowita","Judyta","Julia","Julita","Justyna",
+               "Kamila","Karina","Karolina","Katarzyna","Kazimiera","Kinga","Klaudia","Kleopatra","Kornelia","Krystyna",
+               "Laura","Lena","Leokadia","Lidia","Liliana","Lucyna","Ludmiła","Luiza","Łucja",
+               "Magdalena","Maja","Malwina","Małgorzata","Marcelina","Maria","Marianna","Mariola","Marlena","Marta","Martyna","Marzanna","Marzena","Matylda","Melania","Michalina","Milena","Mirosława","Monika",
+               "Nadia","Natalia","Natasza","Nikola","Nina",
+               "Olga","Oliwia","Otylia",
+               "Pamela","Patrycja","Paula","Paulina",
+               "Regina","Renata","Roksana","Róża","Rozalia",
+               "Sabina","Sandra","Sara","Sonia","Stanisława","Stefania","Stella","Sylwia",
+               "Tamara","Tatiana","Teresa",
+               "Urszula",
+               "Weronika","Wiesława","Wiktoria","Wioletta",
+               "Zofia"," Zuzanna","Zyta","Żaneta" };
+
+            string[] NazwiskaKoncowkiTablica = new string[] { "Kowalsk", "Sosnowsk", "Urbańsk", "Sowińsk", "Kozikowsk" };
+            string[] NazwiskaTablica = new string[] { "Nowak", "Karolczak", "Stępień", "Krawczyk", "Wojtyła", "Pawlacz" };
+            string[] AdresMiastoTablica = new string[]{ "Olsztyn", "Klebark", "Wadowice", "Łęczyca", "Sosnowiec","Klebark Mały", "Węgorzewo","Rybnice", "Radom", "Kraków" };
+            string[] AdresUliceTablica = new string[] { "Kopernika", "Rataja", "Malborska", "Słoneczna", "Watykańska", "Kubusia Puchatka","Dywizjonu 303" };
+            //string[,] array2Db = new string[,] { { "one", "two" }, { "three", "four" },{ "five", "six" } }; //moze sie przydac do przypisywania kodu pocztowego konkretnemu miastu
+
+            string[] EmaileTablica = new string[] { "gmail.com", "wp.pl", "interia.pl", "outlook.com","yahoo.com" };
+            int[] pesel = new int[11];
+            #endregion
+
+            //Stringi do wpisania w pola
+            bool kobieta = true;
+            string wybraneImie;
+            string wybraneNazwisko;
+            string wybranyEmail;
+            string telefonWybrany = "+48";
+            string adresZamieszkaniaWybrany;
+            string adresKorespondencyjnyWybrany;
+
+            //Pora generować!!
+            Random rnd = new Random();
+
+            //Generujemy pesel   
+            for (int i = 0; i < pesel.Length; i++)
+            {
+                pesel[i] = rnd.Next(0, pesel.Length - 1);
+            }
+
+            //jesli 10 liczba jest parzysta to kobieta + wybor imeinia
+            if (pesel[9] % 2 == 0)
+            {
+                kobieta = true;
+                //Wybór imienia
+                wybraneImie = ImionaZenskieTablica[rnd.Next(ImionaZenskieTablica.Length )];
+            }
+            else
+            {
+                kobieta = false;
+                //Wybór imienia
+                wybraneImie = ImionaMeskieTablica[rnd.Next(ImionaMeskieTablica.Length )];
+            }
+
+            //Nazwiska z koncowkami ?? ski/ska?
+            if (rnd.Next(2) == 1)
+            {
+                wybraneNazwisko = NazwiskaKoncowkiTablica[rnd.Next(NazwiskaKoncowkiTablica.Length )];
+                if (kobieta)
+                {
+                    wybraneNazwisko += "a";
+                }
+                else
+                {
+                    wybraneNazwisko += "i";
+                }
+            }
+            else
+            {
+                wybraneNazwisko = NazwiskaTablica[rnd.Next(NazwiskaTablica.Length )];
+            }
+
+            //Jakis email odrazu moze
+            wybranyEmail = wybraneImie + "." + wybraneNazwisko + "@" + EmaileTablica[rnd.Next(EmaileTablica.Length )];
+
+            //Lecimy z telefonem
+            for (int i = 0; i < 9; i++)
+            {
+                telefonWybrany += rnd.Next(0, 10).ToString();
+            }
+            //Adresik np. rataja 54/16, 10-259 Olsztyn
+            adresKorespondencyjnyWybrany = AdresUliceTablica[rnd.Next(AdresUliceTablica.Length)] + " " + rnd.Next(120).ToString() + "/" + rnd.Next(20).ToString() + ", " + rnd.Next(100).ToString() + "-" + rnd.Next(1000).ToString() + " " + AdresMiastoTablica[rnd.Next(AdresMiastoTablica.Length)];
+            //Szansa ze jest taki sam jak zameldowania 1:5
+            if(rnd.Next(5) == 4){
+                adresZamieszkaniaWybrany = adresKorespondencyjnyWybrany;
+            }
+            else
+                adresZamieszkaniaWybrany = AdresUliceTablica[rnd.Next(AdresUliceTablica.Length)] + " " + rnd.Next(120).ToString() + "/" + rnd.Next(20).ToString() + ", " + rnd.Next(100).ToString() + "-" + rnd.Next(1000).ToString() + " " + AdresMiastoTablica[rnd.Next(AdresMiastoTablica.Length)];
+            //Czas wpisac w boxy
+            imieTextBox.Text = wybraneImie;
+            nazwiskoTextBox.Text = wybraneNazwisko;
+            adresKorespondencyjnyTextBox.Text = adresKorespondencyjnyWybrany;
+            adresZameldowaniaTextBox.Text = adresZamieszkaniaWybrany;            
+            telefonTextBox.Text = telefonWybrany;
+            eMailTextBox.Text = wybranyEmail;
+
+            //pesel, moglem odrazu stringa zrobic lol
+            peselTextBox.Text = "";
+            for (int i = 0; i < pesel.Length; i++)
+            {
+                peselTextBox.Text += pesel[i].ToString();
+            }
+            //checkboxy
+            if(rnd.Next(0,22)%2 ==0){
+                checkZgoda1.Checked = true;
+            }
+            else
+                checkZgoda1.Checked = false;
+
+            if (rnd.Next(0, 22) % 3 == 0)
+            {
+                checkZgoda2.Checked = true;
+            }
+            else
+                checkZgoda2.Checked = false;
+
+            if (rnd.Next(0, 22) % 7 == 0)
+            {
+                checkZgoda3.Checked = true;
+            }
+            else
+                checkZgoda3.Checked = false;
+
+            if (rnd.Next(2137) == 1337)
+            {
+                MessageBox.Show("Wirus watykańczyk!");
+            }
+
+
+        }
+        private void NowyKlient_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+            Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Dodaj klienta przycisk
+        {
+                //button2.PerformClick(); //ma sie zamykac okno po dodaniu klienta?
+                //dodajKlientaForm.Close(); //nie wiem ktory lepszy
+                string checkstring1 = "0";
+                string checkstring2 = "0";
+                string checkstring3 = "0";
+                if (checkZgoda1.Checked)
+                    checkstring1 = "1";
+                if (checkZgoda2.Checked)
+                    checkstring2 = "1";
+                if (checkZgoda3.Checked)
+                    checkstring3 = "1";
+
+                string sql = "INSERT INTO klient(imie,nazwisko,pesel,adres_zam,adres_kor,telefon_kon,email,z_przetw,z_market,z_fak,data_utworzenia)";
+                sql += " VALUES (@imie, @nazwisko, @pesel, @adres1, @adres2, @telefon, @email, '"
+                + checkstring1 + "', '"
+                + checkstring2 + "', '"
+                + checkstring3 + "', CURDATE());";
+
+                MySqlCommand cmd = new MySqlCommand(sql, SqlConnectionClass.myConnection);
+                cmd.Parameters.AddWithValue("@imie", imieTextBox.Text);
+                cmd.Parameters.AddWithValue("@nazwisko", nazwiskoTextBox.Text);
+                cmd.Parameters.AddWithValue("@pesel", peselTextBox.Text);
+                cmd.Parameters.AddWithValue("@adres1", adresZameldowaniaTextBox.Text);
+                cmd.Parameters.AddWithValue("@adres2", adresKorespondencyjnyTextBox.Text);
+                cmd.Parameters.AddWithValue("@telefon", telefonTextBox.Text);
+                cmd.Parameters.AddWithValue("@email", eMailTextBox.Text);
+
+                try
+                {
+                    MySqlDataReader rdr = cmd.ExecuteReader();
+                    MessageBox.Show("Dodano klienta do bazy!");
+                    rdr.Close();
+                    button2.PerformClick();
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show("Błąd numer: " + ex.Number + " , " + ex.Message);
+                }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Generuj!
+        {
+            Generacja();
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Zamknij okienko
+        {
+            this.Dispose();
+        }
+
+       
+
+
+    }
+}
